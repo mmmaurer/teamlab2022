dda17d391e102d87bc5dd7d2e373b633ed6bf9fb
mmm <maximilian-maurer@web.de>
2022-05-03 17:12:56 +0200

    Fixed docstrings

--- src/data_representations/data_representations.py
+++ src/data_representations/data_representations.py
@@ -1,16 +1,21 @@
 import typing
 
 class BOW():
+    """Bag of word class represented by sets
+    """
     def __init__(self, text: typing.List[typing.AnyStr]):
         self.rep = set(text)
 
     def similarity(self, other):
-        '''
-        We define similarity as 1 - n^(-1) with n being the number of words in the intersection of two bags of words
+        """We define similarity as 1 - n^(-1) with n being the number of words
+        in the intersection of two bags of words
 
-        Returns: 
-        float: the similarity score within [0,1], the higher, the more similar
-        '''
+        Args:
+            other (BOW): the other bag of words to compare against
+
+        Returns:
+            float: the similarity score within [0,1], the higher, the more similar
+        """
         n = len(self.rep.intersection(other.rep))
         if n == 0: # if no elements in the intersection, just use minimum similarity
             return 0
@@ -18,12 +23,15 @@ class BOW():
             return 1 - (1/n)
 
     def distance(self, other):
-        '''
-        We define similarity as n^(-1) with n being the number of words in the intersection of two bags of words
-        
-        Returns: 
-        float: the distance score within [0,1], the lower, the nearer
-        '''
+        """We define similarity as n^(-1) with n being the number of words
+        in the intersection of two bags of words
+
+        Args:
+            other (BOW): the other bag of words to compare against
+
+        Returns:
+            float: the distance score within [0,1], the lower, the nearer
+        """
         n = len(self.rep.intersection(other.rep))
         if n==0: # if no element in the intersection, just use maximum distance
             return 1
--- src/evaluation/evaluation.py
+++ src/evaluation/evaluation.py
@@ -1,16 +1,14 @@
 import typing
 
 class Evaluator():
-    '''
-    Acts as an evaluator object for a given pair of predictions and their corresponding ground truth
-    '''
+    """Acts as an evaluator object for a given pair of predictions and their corresponding ground truth
+    """
     def __init__(self, gold: typing.List[int], pred: typing.List[int]):
-        '''
-        Types might change, refactoring depending on whether we can use numpy or not
-
-        gold: a list of integers with the index of the true class of an example
-        pred: a list of integers with the index of the predicted class of an example
-        '''
+        """
+        Args:
+            gold (typing.List[int]): a list of integers with the index of the true class of an example
+            pred (typing.List[int]): a list of integers with the index of the predicted class of an example
+        """
         self.gold = gold
         self.pred = pred
         self.n_classes = len(set(gold)) # we need to know the number of classes for micro and macro metrics
@@ -19,12 +17,11 @@ class Evaluator():
 
 
     def instances_per_class(self):
-        '''
-        Calculates TP, FP, TN, FN per class
+        """Calculates TP, FP, TN, FN per class
 
         Returns:
-        tn, fp, tn, fn: dicts with respective (mis-)classifications per class
-        '''
+            tn, fp, tn, fn: dicts with respective number of (mis-)classifications per class
+        """
         tp, fp, tn, fn = {},{},{},{}
         for i in range(self.n_classes):
             # Initializing per class to prevent key errors and checking if key for class is in keys of dict already
@@ -43,53 +40,65 @@ class Evaluator():
         return tn, fp, tn, fn
         
     def accuracy(self):
-        '''
-        Accuracy = TP_total / (TP_total + FN_total + FP_total + TN_total) = TP_total / size_dataset
-        '''
+        """Accuracy = TP_total / (TP_total + FN_total + FP_total + TN_total) 
+                    = TP_total / size_dataset
+
+        Returns:
+            float: accuracy
+        """
         return len([1 for i,j in zip(self.gold, self.pred) if i == j])/len(self.gold) # the length of gold and pred should be equal, so either would be fine here
 
     def precision_per_class(self):
-        '''
-        Precisions per class: TP / (TP + FP)
-        Returns: 
-        dict: precision per class in the gold data
-        '''
+        """Precisions per class: TP / (TP + FP)
+
+        Returns:
+            dict(int:float): precision per class in the gold data
+        """
         # if-condition to prevent division by zero in cases where there is no instance classified as the class
         return {i:self.tp_per_class[i]/(self.tp_per_class[i]+self.fp_per_class[i]) if i in self.pred else 0 for i in range(self.n_classes)}
 
     def recall_per_class(self):
-        '''
-        Recalls per class: TP / (TP + FN)
-        Returns: 
-        dict: recall per class in the gold data
-        '''
+        """Recalls per class: TP / (TP + FN)
+
+        Returns:
+            dict(int:float): recall per class in the gold data
+        """
         # if-condition to prevent division by zero in cases where there is no instance classified as the class
         return {i:self.tp_per_class[i]/(self.tp_per_class[i]+self.fn_per_class[i]) if i in self.pred else 0 for i in range(self.n_classes)}
 
     def fscore_per_class(self):
-        '''
-        '''
+        """F-Score F1 = (2PR)/(P+R) per class
+
+        Returns:
+            dict(int:float): F-Score F1 per glass
+        """
         # if-condition to prevent division by zero in cases where there is no instance classified as the class
         return {i:(2 * self.precision_per_class()[i] * self.recall_per_class()[i]) / (self.precision_per_class()[i] + self.recall_per_class()[i]) if i in self.pred else 0 for i in range(self.n_classes)}
     
     def macro_precision(self):
-        '''
-        For n classes:
+        """For n classes:
         Macro averaged precision = (precision_1 + ... + precision_n) / num_classes
-        '''
+
+        Returns:
+            float: macro averaged precision
+        """
         return sum(self.precision_per_class().values())/self.n_classes
 
     def macro_recall(self):
-        '''
-        For n classes:
+        """For n classes:
         Macro averaged recall = (recall_1 + ... + recall_n) / num_classes
-        '''
+
+        Returns:
+            float: macro averaged recall
+        """
         return sum(self.recall_per_class().values())/self.n_classes
 
     def macro_fscore(self):
-        '''
-        F1 = (2PR)/(P+R)
-        '''
+        """F1 = (2PR)/(P+R)
+
+        Returns:
+            float: macro averaged F-Score
+        """
         # precalculating denominator to prevent division by zero if there's no correctly predicted instance and thus P and R are zero
         denominator = (self.macro_precision() + self.macro_recall())
         if denominator == 0:
@@ -98,23 +107,29 @@ class Evaluator():
             return (2 * self.macro_precision() * self.macro_recall())/denominator
 
     def micro_precision(self):
-        '''
-        For n classes:
+        """For n classes:
         Micro averaged precision = (TP_1 + ... + TP_n) / ((TP_1 + ... + TP_n) + (FP_1 + ... + FP_n))
-        '''
+
+        Returns:
+            float: micro averaged precision
+        """
         return sum(self.tp_per_class.values())/(sum(self.tp_per_class.values()) + sum(self.fp_per_class.values()))
 
     def micro_recall(self):
-        '''
-        For n classes:
+        """For n classes:
         Micro averaged precision = (TP_1 + ... + TP_n) / ((TP_1 + ... + TP_n) + (FN_1 + ... + FN_n))
-        '''
+
+        Returns:
+            float: micro averaged recall
+        """
         return sum(self.tp_per_class.values())/(sum(self.tp_per_class.values()) + sum(self.fn_per_class.values()))
 
     def micro_fscore(self):
-        '''
-        F1 = (2PR)/(P+R)
-        '''
+        """F1 = (2PR)/(P+R)
+
+        Returns:
+            float: micro averages F-Score
+        """
         # precalculating denominator to prevent division by zero if there's no correctly predicted instance and thus P and R are zero
         denominator = (self.micro_precision() + self.micro_recall())
         if denominator == 0:
