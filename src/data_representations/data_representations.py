64c05f8b2c53da8b04629d5c3418ade8129acf8c
mmm <maximilian-maurer@web.de>
2022-05-06 10:00:53 +0200

    Added similarity metrics Jaccard index, Sørensen-Dice coefficient, Tversky index and overlap coefficient to BOW class

--- src/data_representations/data_representations.py
+++ src/data_representations/data_representations.py
@@ -37,7 +37,77 @@ class BOW():
             return 1
         else: 
             return (1/n)
-    
+
+    def jaccard(self, other):
+        """Jaccard index/similarity coefficient for two BOWs
+        J(A,B) = |(A intersection B)| / |(A union B)|
+
+        Args:
+            other (BOW): the other bag of words to compare against
+
+        Returns:
+            float: the Jaccard index
+        """
+        n = len(self.rep.union(other.rep))
+        if n == 0: # both BOWs are empty
+            return 0 # minimum Jaccard index
+        else:
+            return len(self.rep.intersection(other.rep))/n
+
+    def dsc(self, other):
+        """Sørensen-Dice coefficient for two BOWs
+        DSC(A,B) = 2 |(A intersection B)| / (|A| + |B|)
+
+        Args:
+            other (BOW): the other bag of words to compare against
+
+        Returns:
+            float: the DSC index
+        """
+        n = (len(self.rep) + len(other.rep))
+        if n == 0: # both BOWs are empty
+            return 0 # minimum Sørensen-Dice coefficient
+        else:
+            return 2 * len(self.rep.intersection(other.rep))/n
+
+    def tversky(self, other, alpha=1, beta=1):
+        """Tversky index for two BOWs
+        S(A,B) = |(A intersection B)| / (|(A intersection B)| + alpha*(|A-B|) +  beta*(|B-A|)
+
+        alpha=beta=1 -> Jaccard index
+        alpha=beta=0.5 -> Sørensen-Dice coefficient
+        Tversky measures with alpha+beta=1 are of special interest (according to wiki)
+
+        Args:
+            other (BOW): the other bag of words to compare against
+            alpha (int, optional): Defaults to 1.
+            beta (int, optional): Defaults to 1.
+
+        Returns:
+            float: the Tversky index
+        """
+        n = (len(self.rep.intersection(other.rep)) + abs(alpha*len(self.rep - other.rep)) + abs(beta*len(other.rep - self.rep)))
+        if n == 0: # both BOWs are empty
+            return 0 # minimum Tversky index
+        else:
+            return len(self.rep.intersection(other.rep))/n
+
+    def overlap(self, other):
+        """Overlap coefficient for two BOWs
+        overlap(A, B) = |(A intersection B)| / min(|A|,|B|)
+
+        Args:
+            other (BOW): the other bag of words to compare against
+
+        Returns:
+            float: the overlap coefficient
+        """
+        n = min(len(self.rep), len(other.rep))
+        if n == 0: # both BOWs are empty
+            return 0 # minimum overlap coefficient
+        else:
+            return len(self.rep.intersection(other.rep))/n
+
     def __repr__(self):
         return str(self.rep)
 
