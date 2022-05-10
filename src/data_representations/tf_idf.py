340fdd0f60f21c62be6027582aa1dffba372c8cb
Urban Knupleš <urbikn@gmail.com>
2022-05-10 17:25:10 +0200

    Rewrite tf-idf so that it looks more like scikit-learn general design work

--- src/data_representations/tf_idf.py
+++ src/data_representations/tf_idf.py
@@ -1,24 +1,46 @@
 from typing import List
 
 class TfIdf():
-    """Term frequency–inverse document frequency (tf-idf) vector model representation of documents.
+    """Converts a collection of documents into a tf-idf feature matrix representation
     """
 
-    def __init__(self, documents: List[List[str]]):
-        """
+    def __init__(self, documents: List[List[str]]) -> None:
+        self.fit(documents)
+
+
+    def fit(self, documents: List[List[str]]):
+        """Learns idf values from input documents
+
         Args:
-            documents (List[List[str]]): a collection of documents to initiate idf 
+            documents (List[List[str]]): list of documents represented as list of str
+        """
+        if len(documents) == 0:
+            return
+        pass
+
+    def fit_transform(self, documents: List[List[str]]) -> List[List[float]]:
+        """Learns idf values from input documents and returns document-term matrix
+
+        This joins the fit() and transform() methods
+
+        Args:
+            documents (List[List[str]]): list of documents represented as list of str
+
+        Returns:
+            List[List[float]]: tf-idf document-term matrix
+        """
+        self.fit(documents)
+        return self.transform(documents)
+
+    def transform(self, documents: List[List[str]]) -> List[List[float]]:
+        """Transform input documents into document-term matrix using learned idf values
+
+        Args:
+            documents (List[List[str]]): list of documents represented as list of str
+
+        Returns:
+            List[List[float]]: tf-idf document-term matrix
         """
         pass
 
     
-    def vectorize(self, text: List[str]) -> List[float]:
-        """_summary_
-
-        Args:
-            text (List[str]): input text that will be vectorized using tf-idf.
-
-        Returns:
-            List[float]: vectorized representation of input
-        """
-        pass
