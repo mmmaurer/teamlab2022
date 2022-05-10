2755c458e3678515d531a5af3642b5a7661bcb4d
Urban Knupleš <urbikn@gmail.com>
2022-05-10 16:57:29 +0200

    Create skeleton file for tf-idf implementation

---
+++ src/data_representations/tf_idf.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class TfIdf():
+    """Term frequency–inverse document frequency (tf-idf) vector model representation of documents.
+    """
+
+    def __init__(self, documents: List[List[str]]):
+        """
+        Args:
+            documents (List[List[str]]): a collection of documents to initiate idf 
+        """
+        pass
+
+    
+    def vectorize(self, text: List[str]) -> List[float]:
+        """_summary_
+
+        Args:
+            text (List[str]): input text that will be vectorized using tf-idf.
+
+        Returns:
+            List[float]: vectorized representation of input
+        """
+        pass
