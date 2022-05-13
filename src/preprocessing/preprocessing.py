770d468c652dad2684d1d36501945a3fedc85ec7
mmm <maximilian-maurer@web.de>
2022-05-13 19:07:08 +0200

    Moving representation conversion out of preprocessing

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,7 +1,5 @@
 import re
-import sys, os
 
-from data_representations.data_representations import BOW
 
 class Preprocessor():
     def __init__(self, filepath, keep_punc=False, read_limit=1000):
@@ -9,21 +7,27 @@ class Preprocessor():
 
         Args:
             filename (string): path to the data set file
-            keep_punc (bool, optional): Keep punctuation in the tokens-option. Defaults to False.
-            read_limit (int, optional): number of examples to read from file. Defaults to 1000.
+            keep_punc (bool, optional): Keep punctuation in the tokens-option.
+                                        Defaults to False.
+            read_limit (int, optional): number of examples to read from file.
+                                        Defaults to 1000.
         """
         self.keep_punc = keep_punc
-        self.artists, self.titles, self.tokenized_lyrics = self.read(filepath, read_limit)
+        self.artists, self.titles, self.tokenized = self.read(filepath,
+                                                              read_limit)
 
     def read(self, filepath, read_limit=1000):
-        """Read file and tokenize lyrics 
+        """Read file and tokenize lyrics
 
         Args:
             filename (string): path to the data set file
-            read_limit (int, optional): number of examples to read from file. Defaults to 1000.
+            read_limit (int, optional): number of examples to read from file.
+                                        Defaults to 1000.
 
         Returns:
-            list(string), list(string), list(list(string)): list of artists, list of songtitles, list of tokens
+            list(string), list(string), list(list(string)): list of artists,
+                                                            list of songtitles,
+                                                            list of tokens
         """
         artists, titles, tokenized_lyrics = [], [], []
         with open(filepath, 'r') as f:
@@ -31,10 +35,11 @@ class Preprocessor():
                 if i >= read_limit:
                     break
 
-                # The three values for each line are joined together with the tab '\t' character
+                # The three values for each line are joined together with the
+                # tab '\t' character
                 artist, title, lyrics = line.split('\t')
 
-                tokenized = self.tokenize(lyrics) 
+                tokenized = self.tokenize(lyrics)
 
                 artists.append(artist)
                 titles.append(title)
@@ -51,20 +56,13 @@ class Preprocessor():
         Returns:
             list(string): list of words in the lyrics
         """
-        if not self.keep_punc:
-            return re.sub(r'[^\w\s]', '', lyrics.replace(" NEWLINE ", ' ').replace(" NEWLINE\n", '')).split(' ')
+        cleaned = lyrics.replace(" NEWLINE ", ' ').replace(" NEWLINE\n", '')
+        if not self.keep_punc:  # remove punctuation
+            return re.sub(r'[^\w\s]', '', cleaned).split(' ')
         else:
-            return lyrics.replace(" NEWLINE ", ' ').replace(" NEWLINE\n", '').split(' ')
+            return cleaned.split(' ')
 
-    def BOW(self):
-        """Getting a BOW representation of the lyrics
-
-        Returns:
-            list(BOW): list of BOW per lyrics of song in the data set
-        """
-        return [BOW(text) for text in self.tokenized_lyrics]
-
-    def artists(self): 
+    def artists(self):
         """Alias for preprocessor.artists
 
         Returns:
@@ -80,9 +78,8 @@ class Preprocessor():
         """
         return self.titles
 
-if __name__ == "__main__":
-    pre = Preprocessor("/home/mmm/Dropbox/Org/Uni/SS22/teamlab2022/data/songs_train.txt")
-    bow = pre.BOW()
-    print(bow[0].similarity(bow[1]))
-    print(len(pre.artists) == len(pre.titles) == len(pre.tokenized_lyrics))
 
+if __name__ == "__main__":
+    pre = Preprocessor("/home/mmm/Dropbox/Org/Uni/"
+                       "SS22/teamlab2022/data/songs_train.txt")
+    print(len(pre.artists) == len(pre.titles) == len(pre.tokenized))
