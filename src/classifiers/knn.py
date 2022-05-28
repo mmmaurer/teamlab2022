4ed73c0e96c0400d1352f3923cebfbe1972c96a7
Urban Knupleš <urbikn@gmail.com>
2022-05-28 12:02:21 +0200

    Refactor kNN implementation for multiprocessing

--- src/classifiers/knn.py
+++ src/classifiers/knn.py
@@ -3,48 +3,33 @@ from typing import List
 import sys
 import os
 import concurrent.futures
-import multiprocessing
-from itertools import repeat
-import time
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..',
                              'data_representations'))
 from vector import Vector
 
-def _predict(data, targets, inputs: List[Vector], k, measure) -> int:
-    predictions = []
-    for i, input in enumerate(inputs):
-        distances = []
-        # Calculate distance between x and all examples in training set
-        for i, example in enumerate(data):
-            distance = example.distance(input, measure=measure)
-            distances.append([i, distance])
-
-        # Sorts distances and picks k closest examples
-        k_picks = sorted(distances, key=lambda ex: ex[1])[:k]
-
-        # Using saved indexes get corresponding labels
-        labels = [targets[example[0]] for example in k_picks]
-
-        label = max(labels, key=labels.count)
-        predictions.append(label)
-
-    return predictions
 
 class Knn():
     """The K-nearest neighbor classifier for artist classification.
     Using vector representations.
 
-    The algorithm makes the classification
+    This class is implemented to allow for multiprocess execution.
     """
 
-    def __init__(self, input: List[Vector], targets: List[int]) -> None:
+    def __init__(self,
+                 input: List[Vector],
+                 targets: List[int],
+                 multi_process=1) -> None:
         """ TODO: Input typing will change to a general representation.
 
         Args:
             input (typing.List[BOW]): training examples used by the model.
             targets (typing.List[int]): labels corresponding to the training
                                         examples.
+            multi_process (int, optional): number of processes to use when
+                                           predicting. If > 1, the algorithm
+                                           switches to multiprocessing.
+                                           Defaults to 1.
 
         Raises:
             ValueError: raised when targets length not equal to input list
@@ -58,27 +43,60 @@ class Knn():
         # self.data = multiprocessing.Manager().list(input)
         # self.targets = multiprocessing.Manager().list(targets)
 
+        self.multi_process = multi_process
         self.data = input
         self.targets = targets
 
-    def _predict(self, input: Vector, k, measure) -> int:
-        distances = []
-        # Calculate distance between x and all examples in training set
-        for i, example in enumerate(self.data):
-            distance = example.distance(input, measure=measure)
-            distances.append([i, distance])
+    def _predict(self, input: List[Vector], k, measure) -> List[int]:
+        predictions = []
+        for x in input:
+            distances = []
+            # Calculate distance between x and all examples in training set
+            for i, example in enumerate(self.data):
+                distance = example.distance(x, measure=measure)
+                distances.append([i, distance])
 
-        # Sorts distances and picks k closest examples
-        k_picks = sorted(distances, key=lambda ex: ex[1])[:k]
+            # Sorts distances and picks k closest examples
+            k_picks = sorted(distances, key=lambda ex: ex[1])[:k]
 
-        # Using saved indexes get corresponding labels
-        labels = [self.targets[example[0]] for example in k_picks]
+            # Using saved indexes get corresponding labels
+            labels = [self.targets[example[0]] for example in k_picks]
 
-        label = max(labels, key=labels.count)
-        return label
+            label = max(labels, key=labels.count)
+            predictions.append(label)
+      
+        return predictions
+
+    def _predict_multiprocess(self,
+                              input: List[Vector],
+                              k,
+                              measure) -> List[int]:
+        # Function used for splitting input into chunks for processes
+        def chunks(lst, n):
+            """Yield successive n-sized chunks from lst."""
+            for i in range(0, len(lst), n):
+                yield lst[i:i + n]
+
+        chunk_size = int(len(input) / self.multi_process)
+        with concurrent.futures.ProcessPoolExecutor(self.multi_process) as ex:
+            futures = []
+
+            for examples in chunks(input, chunk_size):
+                futures.append(ex.submit(self._predict, examples, k, measure))
+
+            concurrent.futures.wait(futures)
+
+            predictions = list(itertools.chain(
+                *[future.result() for future in futures])
+            )
+
+        return predictions
 
     def predict(self, input: List[Vector], k=5, measure="cosine") -> List[int]:
         """Predict classification for a list of input examples.
+
+        If the value of variable 'multi_process' > 1, the algorithm will make
+        predictions using multiple processes.
 
         Args:
             input (typing.List[BOW]): input examples we want to predict.
@@ -99,21 +117,9 @@ class Knn():
                                                           Vector):
             raise TypeError("Input and model data types are not of same class")
 
-        number_processes = 8
-
-        with concurrent.futures.ProcessPoolExecutor(number_processes) as executor:
-            futures = []
-
-            def chunks(lst, n):
-                """Yield successive n-sized chunks from lst."""
-                for i in range(0, len(lst), n):
-                    yield lst[i:i + n]
-
-            for examples in chunks(input, int(len(input) / number_processes)):
-                futures.append(executor.submit(_predict, self.data, self.targets, examples, k, measure))
-
-            concurrent.futures.wait(futures)
-
-            predictions = list(itertools.chain(*[future.result() for future in futures]))
+        if self.multi_process > 1:
+            predictions = self._predict_multiprocess(input, k, measure)
+        else:
+            predictions = self._predict(input, k, measure)
 
         return predictions
