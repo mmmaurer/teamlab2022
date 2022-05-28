6cf710d2113620ad7f55987c3c1bf0a1b756b7c9
Urban Knupleš <urbikn@gmail.com>
2022-05-28 11:19:08 +0200

    Multiprocessing implemented into KNN algorithm

--- src/classifiers/knn.py
+++ src/classifiers/knn.py
@@ -1,10 +1,35 @@
+import itertools
 from typing import List
 import sys
 import os
+import concurrent.futures
+import multiprocessing
+from itertools import repeat
+import time
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..',
                              'data_representations'))
 from vector import Vector
 
+def _predict(data, targets, inputs: List[Vector], k, measure) -> int:
+    predictions = []
+    for i, input in enumerate(inputs):
+        distances = []
+        # Calculate distance between x and all examples in training set
+        for i, example in enumerate(data):
+            distance = example.distance(input, measure=measure)
+            distances.append([i, distance])
+
+        # Sorts distances and picks k closest examples
+        k_picks = sorted(distances, key=lambda ex: ex[1])[:k]
+
+        # Using saved indexes get corresponding labels
+        labels = [targets[example[0]] for example in k_picks]
+
+        label = max(labels, key=labels.count)
+        predictions.append(label)
+
+    return predictions
 
 class Knn():
     """The K-nearest neighbor classifier for artist classification.
@@ -30,8 +55,27 @@ class Knn():
                     not same dimensions."""
             raise ValueError(error)
 
+        # self.data = multiprocessing.Manager().list(input)
+        # self.targets = multiprocessing.Manager().list(targets)
+
         self.data = input
         self.targets = targets
+
+    def _predict(self, input: Vector, k, measure) -> int:
+        distances = []
+        # Calculate distance between x and all examples in training set
+        for i, example in enumerate(self.data):
+            distance = example.distance(input, measure=measure)
+            distances.append([i, distance])
+
+        # Sorts distances and picks k closest examples
+        k_picks = sorted(distances, key=lambda ex: ex[1])[:k]
+
+        # Using saved indexes get corresponding labels
+        labels = [self.targets[example[0]] for example in k_picks]
+
+        label = max(labels, key=labels.count)
+        return label
 
     def predict(self, input: List[Vector], k=5, measure="cosine") -> List[int]:
         """Predict classification for a list of input examples.
@@ -55,21 +99,21 @@ class Knn():
                                                           Vector):
             raise TypeError("Input and model data types are not of same class")
 
-        predictions = []
-        for x in input:
-            distances = []
-            # Calculate distance between x and all examples in training set
-            for i, example in enumerate(self.data):
-                distance = example.distance(x, measure=measure)
-                distances.append([i, distance])
+        number_processes = 8
 
-            # Sorts distances and picks k closest examples
-            k_picks = sorted(distances, key=lambda ex: ex[1])[:k]
+        with concurrent.futures.ProcessPoolExecutor(number_processes) as executor:
+            futures = []
 
-            # Using saved indexes get corresponding labels
-            labels = [self.targets[example[0]] for example in k_picks]
+            def chunks(lst, n):
+                """Yield successive n-sized chunks from lst."""
+                for i in range(0, len(lst), n):
+                    yield lst[i:i + n]
 
-            label = max(labels, key=labels.count)
-            predictions.append(label)
+            for examples in chunks(input, int(len(input) / number_processes)):
+                futures.append(executor.submit(_predict, self.data, self.targets, examples, k, measure))
+
+            concurrent.futures.wait(futures)
+
+            predictions = list(itertools.chain(*[future.result() for future in futures]))
 
         return predictions
